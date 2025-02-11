a8ab2cf147ca6955d28206843629e508796b6c1b
Simran kumari <simrangupta2403@gmail.com>
2025-02-11 18:59:57 +0530

    js Datatype file commited

---
+++ 02_Datatypes.js
@@ -0,0 +1,28 @@
+"use strict" ; // Treat all JS code as newer version
+
+//alert(3+3) // We are using nodejs not Browser
+
+//Code readibility should be high
+let name="Simran"
+let age=35
+let isLoggedIn=false
+
+//number 2 to power 53 
+//biginit use big data
+//String => " "
+//boolean => False/True
+//null = empty// standlone value
+//Undefined : value not defined 
+//object 
+//symbol :> unique value
+console.log(typeof name)
+
+console.log("null type is :"+ typeof null)
+
+//Null type is null and it's type is object
+
+
+
+
+
+
